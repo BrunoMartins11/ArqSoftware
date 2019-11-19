7c16c4c6e35bba7da100e4af143a18c6932334fb
Bruno Martins <brunocmartins@live.com.pt>
2019-11-19 10:56:55 +0000

    Tweak daos and make esstrading constructor

--- src/BusinessModel/ESSTrading.java
+++ src/BusinessModel/ESSTrading.java
@@ -4,16 +4,44 @@ import BusinessModel.Report.Bug;
 import BusinessModel.User.*;
 import BusinessModel.Assets.*;
 import BusinessModel.Trading.*;
+import Data.AssetDAO;
+import Data.BugDAO;
+import Data.CFDdao;
+import Data.UserDAO;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class ESSTrading {
 
-	private Map<String, User> users;
-	private Map<String, Asset> assets;
+	private Map<Integer, User> users;
+	private Map<Integer, Asset> assets;
 	private Map<Integer, CFD> cfds;
 	private List<Bug> bugs;
+
+	public ESSTrading(){
+
+		users = new HashMap<>();
+		UserDAO userDAO = new UserDAO();
+		for (User user: userDAO.getAll()) {
+			users.put(user.getId(), user);
+		}
+
+		assets = new HashMap<>();
+		AssetDAO assetDAO = new AssetDAO();
+		for(Asset asset: assetDAO.getAll()){
+			assets.put(asset.getId(), asset);
+		}
+
+		cfds = new HashMap<>();
+		CFDdao cfddao = new CFDdao();
+		for (CFD cfd: cfddao.getAll()){
+			cfds.put(cfd.getId(), cfd);
+		}
+
+		bugs = (new BugDAO()).getAll();
+	}
 
 	public Map<String, User> getUsers() {
 		throw new UnsupportedOperationException();
