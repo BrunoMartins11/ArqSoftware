cc386cc51752edfefadaf68860d3458c3878c96b
Bruno Martins <brunocmartins@live.com.pt>
2019-11-23 16:04:07 +0000

    Add functional admin page

--- src/Presentation/Admin/AdminFacade.java
+++ src/Presentation/Admin/AdminFacade.java
@@ -1,7 +1,10 @@
 package Presentation.Admin;
 
 import BusinessModel.ESSTrading;
+import BusinessModel.Report.Bug;
 import Presentation.Shared.SharedFacade;
+
+import java.util.List;
 
 public class AdminFacade
 {
@@ -22,6 +25,19 @@ public class AdminFacade
 
     public void openMainMenu()
     {
+        mainMenu = new MainMenu();
+        mainMenu.drawMainMenu();
+        setAuthentication(true);
+        int scanned = mainMenu.intInput();
+        if(scanned == 1){
+            sharedFacade.openStocksMenu();
+        }
+        if(scanned == 2) {
+            List<Bug> bugs= essTrading.getBugs();
+            for (Bug b: bugs) {
+                System.out.println(b.getId() + ". " + b.getError());
+            }
+        }
         mainMenu.drawMainMenu();
     }
 
